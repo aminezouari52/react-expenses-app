115ec87d52fa0accdc028e6645820c592eb4aa68
amine <zouariamine52@gmail.com>
2022-06-15 11:32:09 +0100

    build

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+
+import Card from '../UI/Card'
+import ExpensesFilter from './ExpensesFilter'
+import './Expenses.css'
+import ExpensesList from './ExpensesList'
+import ExpensesChart from './ExpensesChart'
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020')
+
+  const filterChangeHandeler = (selectedYear) => {
+    setFilteredYear(selectedYear)
+  }
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear
+  })
+
+  return (
+    <li>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandeler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </li>
+  )
+}
+
+export default Expenses
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+import ExpenseForm from './ExpenseForm'
+import './NewExpense.css'
+
+const NewExpense = (props) => {
+  const saveExpenseDataHandeler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    }
+    props.onAddExpense(expenseData)
+    setIsEditing(false)
+  }
+
+  const [isEditing, setIsEditing] = useState(false)
+
+  const startEditingHandler = () => {
+    setIsEditing(true)
+  }
+
+  const stopEditing = () => {
+    setIsEditing(false)
+  }
+
+  return (
+    <div className="new-expense">
+      {!isEditing && (
+        <button onClick={startEditingHandler}>add new expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandeler}
+          onCancel={stopEditing}
+        />
+      )}
+    </div>
+  )
+}
+
+export default NewExpense
